159972d83cc8ba13af88db1365ad1bcd24c7ecbc
Aymeric Ponjée <aymericponjee@gmail.com>
2020-04-09 00:07:36 +0200

    update

--- DivinSauveurApp/App.js
+++ DivinSauveurApp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yo gros mec</Text>
+      <Text>Yo gros mec ça va ? </Text>
     </View>
   );
 }
