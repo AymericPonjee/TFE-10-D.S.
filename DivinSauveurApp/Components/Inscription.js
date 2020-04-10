e04ea38451297201464c0d807b5a43e5cafe7d79
Aymeric Ponjée <aymericponjee@gmail.com>
2020-04-10 12:54:23 +0200

    Update Inscription.js

--- DivinSauveurApp/Components/Inscription.js
+++ DivinSauveurApp/Components/Inscription.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import { View, Button, TextInput, Text } from 'react-native'
+import { StyleSheet, View, Button, TextInput } from 'react-native'
 
 class Inscription extends React.Component{
     render(){
         return(
-            <View>
-                <TextInput placeholder="Pseudo"/>
-                <Button title="Inscription" onPress={() => {}}/>
-                <Text>Contact</Text>
+            <View style={styles.main}>
+                <TextInput style={styles.pseudo} placeholder="Pseudo"/>
+                <Button style={{ height:50 }} title="Inscription" onPress={() => {}}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    main:{
+        marginTop:50,
+        flex:1
+    },
+
+    pseudo: {
+        marginLeft:5,
+        marginRight:5,
+        height:50,
+        borderColor: '#000000',
+        borderWidth:1,
+        paddingLeft:5
+    }
+})
 
 export default Inscription
